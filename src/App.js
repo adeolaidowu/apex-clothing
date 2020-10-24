8cc5b0abacf5f664c974bdb0884b6bdb856ff77b
Adeola idowu <adeolaidowu27@gmail.com>
2020-10-24 15:52:41 +0100

    add header component to render on all routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import './App.css';
+import Header from './components/Header';
 import HomePage from './components/HomePage';
 import ShopPage from './components/ShopPage';
 
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route path='/' component={HomePage} exact />
       <Route path='/shop' component={ShopPage} />
