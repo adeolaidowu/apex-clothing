554fbe45f00343ce10c7ee40c128f561a234d65d
Adeola idowu <adeolaidowu27@gmail.com>
2021-03-01 14:41:02 +0100

    add payment integration

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../selectors/cart';
+import StripeCheckoutButton from '../components/StripeButton';
 import CheckoutItem from '../components/CheckoutItem';
 
 const CheckoutPage = ({cartItems, total}) => (
@@ -29,6 +30,12 @@ const CheckoutPage = ({cartItems, total}) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br/>
+      4242424242424242 - Exp: 05/23 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 )
 
